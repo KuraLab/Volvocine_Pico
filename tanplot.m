59e824973302a817f2baa50a04c1399533978fb1
nishikawak529 <nishikawa.k.al@m.titech.ac.jp>
2025-07-31 15:07:01 +0900

    tanplot.mのプロットを修正し、x軸とy軸の設定をπベースに変更しました。abs関数を使用してphase2の絶対値をプロットするようにしました。

--- tanplot.m
+++ tanplot.m
@@ -5,10 +5,20 @@ phase2 = [-0.129 -0.091 -0.032 -0.195 -0.099 -0.101 -0.054 -0.196 -0.307 -0.306 
 
 % プロット
 figure;
-plot(alpha, phase2, '-o', 'LineWidth', 2);
+plot(alpha, abs(phase2), '-o', 'LineWidth', 2);
 xlabel('\alpha');
 ylabel('\phi_2 - \phi_1');
 xlim([-pi pi])
 ylim([-pi pi])
+
+% x軸をπベースで設定
+xticks([-pi -pi/2 0 pi/2 pi]);
+xticklabels({'-\pi', '-\pi/2', '0', '\pi/2', '\pi'});
+
+% y軸をπベースで設定
+yticks([-pi -pi/2 0 pi/2 pi]);
+yticklabels({'-\pi', '-\pi/2', '0', '\pi/2', '\pi'});
+
 grid on;
 tuneFigure;
+saveFigure;
