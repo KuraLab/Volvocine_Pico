9896f2e71a3d3167d97d44e70eeb37aa473e1608
nishikawak529 <nishikawa.k.al@m.titech.ac.jp>
2025-12-03 16:46:17 +0100

    ex 2/N

--- plot_phase_evolution_omega245.m
+++ plot_phase_evolution_omega245.m
@@ -14,7 +14,7 @@ save%   apply_filter = true
 %   do_save_figure = false
 
     if nargin < 1 || isempty(dirpath)
-        dirpath = fullfile('EstimateF','Spring3/250');
+        dirpath = fullfile('EstimateF','Spring3/255');
     end
     if nargin < 2 || isempty(n_seconds_to_cut)
         n_seconds_to_cut = 0;0
