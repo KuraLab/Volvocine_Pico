29621cb12c44d834ad5becd7503a1f76edf8cb92
nishikawak529 <nishikawa.k.al@m.titech.ac.jp>
2026-05-07 15:57:46 +0200

    experiment 7 May

--- ServerResponse.py
+++ ServerResponse.py
@@ -5,7 +5,7 @@ import os
 import re
 
 omega_values = {
-    4: 3.14 * 2.60   # エージェント1の周波数
+    4: 3.14 * 2.54   # エージェント1の周波数
 }
 default_omega = 3.14 * 2.50 # デフォルト周波数（未定義IDの場合）
 
@@ -22,7 +22,7 @@ PRC_SOURCE_DIR = os.environ.get(
     "PRC_SOURCE_DIR",
     os.path.join("EstimateQ", "Spring3", "255", "gamma_exports")
 )
-PRC_SOURCE_FILE = "prc_snippet_ref_cos.txt"
+PRC_SOURCE_FILE = "prc_snippet_ref_w1.txt"
 
 
 def load_prc_from_directory(source_dir, source_file=PRC_SOURCE_FILE):
